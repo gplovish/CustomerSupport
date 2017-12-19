bf051a0e30acf5efbad10baccd1fb800fb6b080f
lovishdureja <lovish.dureja@globalpueblosolutions.com>
2017-12-19 15:05:18 +0530

    initial commit

---
+++ CustomerSupportApp/SplashActivity.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Threading;
+
+namespace CustomerSupportApp
+{
+    [Activity(Label = "CustomerSupportApp", MainLauncher = true, Theme = "@style/Theme.Splash")]
+    public class SplashActivity : Activity
+    {  
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Thread.Sleep(500);
+            StartActivity(typeof(MainActivity));
+        }
+
+        public override void OnBackPressed() { }
+
+        
+    }
+}
---
+++ CustomerSupportApp/obj/Debug/android/src/md5840b80b99c55e3088feed827926b4a9e/SplashActivity.java
@@ -0,0 +1,56 @@
+package md5840b80b99c55e3088feed827926b4a9e;
+
+
+public class SplashActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onBackPressed:()V:GetOnBackPressedHandler\n" +
+			"";
+		mono.android.Runtime.register ("CustomerSupportApp.SplashActivity, CustomerSupportApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", SplashActivity.class, __md_methods);
+	}
+
+
+	public SplashActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == SplashActivity.class)
+			mono.android.TypeManager.Activate ("CustomerSupportApp.SplashActivity, CustomerSupportApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onBackPressed ()
+	{
+		n_onBackPressed ();
+	}
+
+	private native void n_onBackPressed ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
